f924bc31afdacf35d6ca1e00c460ca09c65b80aa
Abdullah Eroglu <abdullah@maviucak.com>
2019-01-07 15:32:31 +0300

    added mysql export

--- mysql/export-csv.sh
+++ mysql/export-csv.sh
@@ -1,4 +1,4 @@
 echo "$1"
 echo "$2"
 mysql -h hostname -u username -P 3306 -ppassword --batch -e "select * from $1 where field = 'bla' limit 10" |
-sed 's/d_sales_tab/","/g'| sed 's/^/"/g' | sed 's/$/"/g' | sed 's/\n//g' > export.csv
+sed 's/tablename/","/g'| sed 's/^/"/g' | sed 's/$/"/g' | sed 's/\n//g' > export.csv
